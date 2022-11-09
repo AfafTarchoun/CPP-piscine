e04b61c81cfedd2680907fb5dca11fe001b9af70
Afaf Tarchoun <git@vogsphere-v2-bg.1337.ma>
2022-11-09 18:43:10 +0100

    start2

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: atarchou <atarchou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:46:12 by atarchou          #+#    #+#             */
-/*   Updated: 2022/11/08 17:50:56 by atarchou         ###   ########.fr       */
+/*   Updated: 2022/11/09 17:35:44 by atarchou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ class PhoneBook
 {
     Contact contact[8];
     int index;
+
     public:
         PhoneBook (void);
         ~PhoneBook (void);
--- ex01/ex01.cpp
+++ ex01/ex01.cpp
@@ -6,7 +6,7 @@
 /*   By: atarchou <atarchou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 07:51:29 by atarchou          #+#    #+#             */
-/*   Updated: 2022/11/08 17:54:06 by atarchou         ###   ########.fr       */
+/*   Updated: 2022/11/09 17:53:06 by atarchou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,21 @@
 //     return;
 // }
 
+void    add(void)
+{
+    std::string name;
 
+    std::cout << "Please, enter your first name: " ;
+    std::getline (std::cin,name);
+    std::cout << "Please, enter your last name: ";
+    std::getline (std::cin,name);
+    std::cout << "Please, enter your nickname: ";
+    std::getline (std::cin,name);
+    std::cout << "Please, enter your phone number: ";
+    std::getline (std::cin,name);
+    std::cout << "Please, enter your darkest secret: ";
+    std::getline (std::cin,name);
+}
 
 int main()
 {
@@ -38,20 +52,22 @@ int main()
 
     // std::cout << "Please, enter your full name: ";
     // std::getline (std::cin,name);
+    char *input; 
     std::cout << "Please enter :ADD, SEARCH, EXIT" << std::endl;
-    // while (1)
-    // {
-    //     std::cin >>;
-    //     if (std::strcmp("ADD",) == 0)
-    //     {
-    //         std::cout<< "lol" << std::endl;
-    //     }
-    //     else if (std::strcmp("SEARCH", ) == 0)
-    //     {
-    //         std::cout<<"ha"<<std::endl;
-    //     }
-    //     else if (std::strcmp("EXIT",) == 0)
-    //         return 0;
-    // }
+    while (1)
+    {
+        std::cin >> input;
+        if (std::strcmp("ADD",input) == 0)
+        {
+            // std::cout<< "lol" << std::endl;
+            add();
+        }
+        else if (std::strcmp("SEARCH", input) == 0)
+        {
+            std::cout<<"ha"<<std::endl;
+        }
+        else if (std::strcmp("EXIT", input) == 0)
+            return 0;
+    }
     return 0;
 }
