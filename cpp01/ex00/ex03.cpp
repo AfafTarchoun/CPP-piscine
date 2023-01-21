e8ddca57948b832ac30c10684bd83fec70fc3ae5
AfafTarchoun <74008660+AfafTarchoun@users.noreply.github.com>
2023-01-21 16:48:03 +0100

    Update ex03.cpp

--- cpp01/ex00/ex03.cpp
+++ cpp01/ex00/ex03.cpp
@@ -12,3 +12,62 @@
 
 #include <iostream>
 #include <string>
+
+class Weapon {
+    private:
+        string type;
+    public:
+        Weapon(string t) : type(t) {}
+        const string& getType() const {return type;}
+        void setType(string t) {type = t;}
+};
+
+class HumanA {
+    private:
+        Weapon weapon;
+        string name;
+    public:
+        HumanA(string n, Weapon w) : name(n), weapon(w) {}
+        void attack() {cout << name << " attacks with their " << weapon.getType() << endl;}
+};
+
+class HumanB {
+    private:
+        Weapon* weapon;
+        string name;
+    public:
+        HumanB(string n) : name(n), weapon(nullptr) {}
+        ~HumanB() {delete weapon;}
+        void setWeapon(Weapon w) {
+            if (weapon != nullptr) {
+                delete weapon;
+            }
+            weapon = new Weapon(w);
+        }
+        void attack() {
+            if (weapon != nullptr) {
+                cout << name << " attacks with their " << weapon->getType() << endl;
+            } else {
+                cout << name << " has no weapon to attack with" << endl;
+            }
+        }
+};
+
+int main() {
+    {
+        Weapon club("crude spiked club");
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
+    {
+        Weapon club("crude spiked club");
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
+    return 0;
+}
